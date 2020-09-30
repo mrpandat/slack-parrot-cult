904a5adad6571fc0ee71f7d06c5fe09408ae8e08
Juan <mrpandat@hotmail.fr>
2020-09-30 11:42:40 +0200

    Print url and name of emojis.

--- parroted.py
+++ parroted.py
@@ -3,5 +3,8 @@ host = 'https://cultofthepartyparrot.com'
 
 f = open('inventory')
 for line in f:
-    print(host + line)
+    emojiName = line.split('.gif')[0].split('/')[-1]
+    emojiUrl = host + line
+    print('name: ' + emojiName)
+    print('url: ' + emojiUrl)
 f.close()
